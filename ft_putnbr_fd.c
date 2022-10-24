3b8fecbde7646f5c823023333de171e2cd47fd0d
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-25 00:15:55 +0200

    re-implmentation of ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,37 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:44:52 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/18 13:46:44 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/25 00:13:51 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	num_digits(long n)
+{
+	size_t	result;
+
+	result = 1;
+	while (n > 10)
+	{
+		n /= 10;
+		result++;
+	}
+	return (result);
+}
+
+static long	pow_10(size_t e)
+{
+	long	result;
+
+	result = 1;
+	while (e > 1)
+	{
+		result *= 10;
+		e--;
+	}
+	return (result);
+}
 
 /*
 	Outputs the integer ’n’ to the given file descriptor.
@@ -20,5 +46,27 @@
 */
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	long	divisor;
+	long	tmp;
+	char	c;
+	size_t	digits;
+
+	tmp = (long)(n);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', 1);
+		tmp = -1 * (long)(n);
+	}
+	digits = num_digits(tmp);
+	divisor = pow_10(digits);
+	while (digits--)
+	{
+		if (tmp < 10)
+			c = (char)(tmp + 48);
+		else
+			c = (char)((tmp / divisor) + 48);
+		ft_putchar_fd(c, fd);
+		tmp = tmp % divisor;
+		divisor /= 10;
+	}
 }
