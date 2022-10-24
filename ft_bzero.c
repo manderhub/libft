f41d0a6f9e7cb619f5fb6f698848cb73a996c5e1
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-24 23:03:34 +0200

    bugfix ft_calloc and simplify ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,11 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:45:29 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/18 16:38:16 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/24 23:02:33 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// CAN PROBAbLY USE ft_memset for this
 
 /*
 	Writes n zero bytes ('\0') to the string s.
@@ -20,18 +18,6 @@
 	s:	destination string
 */
 void	ft_bzero(void *s, size_t n)
-{
-	char	*s_char;
-
-	s_char = (char *)(s);
-	while (n--)
-	{
-		*s_char = '\0';
-		s_char++;
-	}
-}
-
-void	ft_bzero_alt(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 18:43:15 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/15 19:09:40 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/24 23:01:28 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 	Contiguously allocates enough space for count objects that are size bytes
-	of memory each and filles the allocated memory with bytes of value zero.
+	of memory each and fills the allocated memory with bytes of value zero.
 	
 	count:		number of objects
 	size:		size of each object
@@ -28,6 +28,6 @@ void	*ft_calloc(size_t count, size_t size)
 	buf = malloc(count * size);
 	if (buf == NULL)
 		return (NULL);
-	ft_bzero(buf, count);
+	ft_bzero(buf, count * size);
 	return (buf);
 }
