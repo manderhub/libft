2e2bcbc58e050fc8660113e4c414395de73f9784
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-26 15:28:09 +0200

    implements ft_lstmap, but too many lines

---
+++ ft_lstmap.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 13:09:11 by manderhu          #+#    #+#             */
+/*   Updated: 2022/10/26 15:25:25 by manderhu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * Iterates the list 'lst' and applies the function 'f' on the content of
+ * each node. Creates a new list resulting of the successive applications
+ * of the function 'f'. The 'del' function is used to delete the content of
+ * a node if needed.
+ * 
+ * lst:		pointer to a node
+ * f:		address of the function used to iterate on the list
+ * del:		address of the function used to delete the content of a node
+ * returns:	the new list; NULL if allocation fails
+*/
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*start;
+	t_list	*new_node;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	start = NULL;
+	while (lst)
+	{
+		if (start == NULL)
+		{
+			start = ft_lstnew((*f)(lst->content));
+			if (start == NULL)
+				return (NULL);
+		}
+		else
+		{
+			new_node = ft_lstnew((*f)(lst->content));
+			if (new_node == NULL)
+			{
+				ft_lstclear(&start, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&start, new_node);
+		}
+		lst = lst->next;
+	}
+	return (start);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 16:45:50 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/26 11:55:32 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/26 14:32:21 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,4 +64,5 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
