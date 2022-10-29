e2dae76a9513fff82f31f1c7954263ff9b739bc9
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-29 16:37:30 +0200

    using ft_memset instead of ft_bzero

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 18:43:15 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/27 12:03:24 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/29 16:02:15 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
  * Contiguously allocates enough space for count objects that are size bytes
  * of memory each and fills the allocated memory with bytes of value zero.
  * 
- * count:		number of objects
- * size:		size of each object
+ * count:	number of objects
+ * size:	size of each object
  * returns:	pointer to zero-filled allocated memory; on error NULL
  * 			is returned
 */
@@ -30,6 +30,6 @@ void	*ft_calloc(size_t count, size_t size)
 	buf = malloc(count * size);
 	if (!buf)
 		return (NULL);
-	ft_bzero(buf, count * size);
+	ft_memset(buf, 0, count * size);
 	return (buf);
 }
