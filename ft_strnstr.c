babb9417c03e40e315650388d417d778d390f9e8
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-29 12:33:22 +0200

    bugfix size_t overflow due to substraction resulting in negative values -> overflow

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,15 +6,15 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 14:47:53 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/27 15:28:33 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/29 12:28:07 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
- * Locates the first occurrence of string needle in string haystack.
- * Not more than len characters are searched and characters after '\0'
+ * Locates the first occurrence of string 'needle' in string 'haystack'.
+ * Not more than 'len' characters are searched and characters after '\0'
  * are also not searched.
  * 
  * haystack:	string to be searched in
@@ -28,23 +28,22 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*result;
 	size_t	len_needle;
 
 	len_needle = ft_strlen(needle);
 	if (!len_needle)
 		return ((char *) haystack);
-	if (!len)
-		return (NULL);
 	i = 0;
-	while (i <= len - len_needle && *(haystack + i))
+	while (*(haystack + i) && (i + len_needle) <= len)
 	{
-		result = (char *)(haystack + i);
 		j = 0;
-		while (*(haystack + i + j) == *(needle + j) && *(needle + j))
+		while (*(haystack + i + j) && *(needle + j)
+			&& *(haystack + i + j) == *(needle + j))
+		{
 			j++;
-		if (!*(needle + j))
-			return (result);
+			if (j == len_needle)
+				return ((char *)(haystack + i));
+		}
 		i++;
 	}
 	return (NULL);
