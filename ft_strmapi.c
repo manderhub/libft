85841c266030746d5491100d9f41b2e46c0cd6f3
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-29 17:33:03 +0200

    using ft_calloc instad of malloc

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,20 +6,20 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 12:17:24 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/27 15:21:01 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/29 17:31:45 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
- * Applies the function ’f’ to each character of the string ’s’,
- * and passing its index as first argument to create a new string
- * resulting from successive applications of ’f’.
+ * Applies the function 'f' to each character of the string 's',
+ * and passing the character's index as first argument to create a new string
+ * resulting from successive applications of 'f'.
  * 
  * s:		string on which to iterate
  * f:		function to apply to each character
- * returns:	the string created from successive applications of f
+ * returns:	the string created from successive applications of 'f'
 */
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
@@ -28,7 +28,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 
 	if (!s || !f)
 		return (NULL);
-	result = malloc((ft_strlen(s) + 1) * sizeof(*result));
+	result = ft_calloc(ft_strlen(s) + 1, sizeof(*result));
 	if (result == NULL)
 		return (NULL);
 	i = 0;
@@ -37,6 +37,5 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		*(result + i) = f(i, *(s + i));
 		i++;
 	}
-	*(result + i) = '\0';
 	return (result);
 }
