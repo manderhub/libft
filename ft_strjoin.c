63cb5f985ed5793bda3fc61ef97b0bc013fcb773
Mario Anderhub <manderhu@student.42heilbronn.de>
2022-10-29 17:20:58 +0200

    using ft_calloc instad of malloc

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: manderhu <manderhu@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 10:33:33 by manderhu          #+#    #+#             */
-/*   Updated: 2022/10/27 15:05:24 by manderhu         ###   ########.fr       */
+/*   Updated: 2022/10/29 17:16:07 by manderhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 /*
  * Allocates and returns a new string, which is the result of the
- * concatenation of ’s1’ and ’s2’.
+ * concatenation of 's1' and 's2'.
  * 
- * s1:			prefix string
- * s2:			suffix string
+ * s1:		prefix string
+ * s2:		suffix string
  * returns:	the new concatenated string; NULL if allocation fails
 */
 char	*ft_strjoin(char const *s1, char const *s2)
@@ -30,11 +30,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
-	concatenation = malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	concatenation = ft_calloc(len_s1 + len_s2 + 1, sizeof(char));
 	if (concatenation == NULL)
 		return (NULL);
 	ft_memmove(concatenation, s1, len_s1);
 	ft_memmove(concatenation + len_s1, s2, len_s2);
-	*(concatenation + len_s1 + len_s2) = '\0';
 	return (concatenation);
 }
